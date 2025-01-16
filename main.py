fca15bb81bae11d3a3efddecc3ad70f505ac5880
Stormyisland <jacqueslavoie2015@gmail.com>
2025-01-16 00:49:41 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __int__(self):
+        super().__init__()
+        
+        
+def main():
+    pass
+
+def main():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
